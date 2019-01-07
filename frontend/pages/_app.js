9bc88ca70c7935a18fa78469b66d6114d17358ce
liulin <liulin1987315@gmail.com>
2019-01-08 06:10:28 +1100

    Add page component.

---
+++ frontend/pages/_app.js
@@ -0,0 +1,15 @@
+import App, { Container } from 'next/app';
+
+class MyApp extends App {
+    render() {
+        const { Component } = this.props;
+        return (
+            <Container>
+                <p>hello, I am on every page!</p>
+                <Component />
+            </Container>
+        )
+    }
+};
+
+export default MyApp;
