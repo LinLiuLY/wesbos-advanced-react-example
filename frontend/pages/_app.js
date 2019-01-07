b98060c05b5eb5f5d200d10ae1f00ce43521e9d1
liulin <liulin1987315@gmail.com>
2019-01-08 06:14:01 +1100

    Add prettier to format code.

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,15 +1,15 @@
 import App, { Container } from 'next/app';
 
 class MyApp extends App {
-    render() {
-        const { Component } = this.props;
-        return (
-            <Container>
-                <p>hello, I am on every page!</p>
-                <Component />
-            </Container>
-        )
-    }
-};
+  render() {
+    const { Component } = this.props;
+    return (
+      <Container>
+        <p>hello, I am on every page!</p>
+        <Component />
+      </Container>
+    );
+  }
+}
 
 export default MyApp;
